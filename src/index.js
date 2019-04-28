999e6aeeb6f9efc07102a8820877da93f245cef4
Pob <590650@gmail.com>
2019-04-29 01:06:23 +0700

    create grid table

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import './style.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="grid">
+        {(() => {
+          const blocks = []
+          let i = 1
+          while (i++ <= 625) {
+            blocks.push(<div className="block" key={i} />)
+          }
+          return blocks
+        })()}
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
