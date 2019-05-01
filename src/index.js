78b397ae82ecaf26f7d36eed73905989cc4efb62
Pob <590650@gmail.com>
2019-05-01 23:40:41 +0700

    done on undo function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
+import { useHistory } from './useHistory'
+
+const initValue = Array(625).fill(false)
 
 function App() {
+  const { present, setPresent, undo, canUndo } = useHistory(initValue)
   return (
     <div className="container">
+      <div>
+        <button onClick={undo} disabled={!canUndo}>
+          Undo
+        </button>
+        <button>Redo</button>
+        <button>Reset</button>
+      </div>
       <div className="grid">
         {(() => {
           const blocks = []
-          let i = 1
-          while (i++ <= 625) {
-            blocks.push(<div className="block" key={i} />)
+          let i = 0
+          while (i++ < 625) {
+            const index = i // for closure in onClick callback function
+            blocks.push(
+              <div
+                className={`block${present[i] ? ' active' : ''}`}
+                key={i}
+                onClick={() => {
+                  const newPresent = [...present]
+                  newPresent[index] = !newPresent[index]
+                  setPresent(newPresent)
+                }}
+              />
+            )
           }
           return blocks
         })()}
---
+++ src/useHistory.js
@@ -0,0 +1,43 @@
+import { useReducer } from 'react'
+
+const initialState = {
+  past: [], // array of present array
+  present: [], // array of boolean
+  future: [] // array of present array
+}
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'UNDO':
+      return {
+        past: prevState.past.slice(0, prevState.past.length - 1),
+        present: prevState.past[prevState.past.length - 1],
+        future: [...prevState.future, prevState.present]
+      }
+    case 'REDO':
+    case 'SET':
+      return {
+        past: [...prevState.past, prevState.present],
+        present: action.newPresent,
+        future: []
+      }
+    default:
+      throw new Error()
+  }
+}
+
+export const useHistory = initPresent => {
+  const [state, dispatch] = useReducer(reducer, { ...initialState, present: initPresent })
+
+  const setPresent = newPresent => {
+    dispatch({ type: 'SET', newPresent })
+  }
+
+  const undo = () => {
+    dispatch({ type: 'UNDO' })
+  }
+
+  const canUndo = state.past.length > 0
+
+  return { present: state.present, setPresent, undo, canUndo }
+}
